9e7743ded08af966ee30ce63d2a90caa9c2451eb
Dinh Bac <nguyendinhbac99@gmail.com>
2019-09-11 15:51:22 +0700

    init homebrokers

---
+++ homebrokers/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function(done) {
+    browserSync.init({
+        server: {
+            proxy:"localhost:8010",
+            baseDir: './',
+            open:true,
+            online: false,
+            ghostMode: {
+                scroll: true
+            },
+            logLevel: 'info', // 'debug', 'info', 'silent', 'warn'
+            logConnections: false,
+            logPrefix: "Browser-Sync",
+            notify: true
+        },
+    });
+    done();
+});
+
+gulp.task('sass', function(){
+  	return gulp.src('./assets/sass/**/*.scss')
+    	.pipe(sass().on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
+    	.pipe(gulp.dest('./assets/css'))
+    	.pipe(browserSync.reload({
+      		stream: true
+	    }))
+});
+
+gulp.task('watch', ['sass', 'browserSync'], function(){
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
+    gulp.watch('./**/*.html', browserSync.reload); 
+    gulp.watch('./assets/js/**/*.js', browserSync.reload);
+})
+
+// skip if error occured
+function swallowError(error) {
+    console.log(error.toString())
+    this.emit('end')
+}
