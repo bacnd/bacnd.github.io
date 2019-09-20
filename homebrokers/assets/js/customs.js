a6805339b58cadae74eb893ab379fdd66e2fdeef
Dinh Bac <nguyendinhbac99@gmail.com>
2019-09-20 17:38:31 +0700

    dev dong san moi nhat

--- homebrokers/assets/js/customs.js
+++ homebrokers/assets/js/customs.js
@@ -67,6 +67,16 @@ $(document).ready(function() {
         $(this).toggleClass('active');
         $(this).next().toggleClass('active');
     });
+
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    })
 });
 
 $(function() {
