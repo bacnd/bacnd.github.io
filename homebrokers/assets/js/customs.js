176ccce7bf4878524c0ba0fb890696c498076bec
Dinh Bac <nguyendinhbac99@gmail.com>
2019-10-03 04:39:52 +0700

    dev page 7

--- homebrokers/assets/js/customs.js
+++ homebrokers/assets/js/customs.js
@@ -198,6 +198,32 @@ $(function() {
     //         }, 1200);
     // });
 
+    $('input[type="range"]').rangeslider({
+
+        // Feature detection the default is `true`.
+        // Set this to `false` if you want to use
+        // the polyfill also in Browsers which support
+        // the native <input type="range"> element.
+        polyfill: true,
+
+        // Default CSS classes
+        rangeClass: 'rangeslider',
+        disabledClass: 'rangeslider--disabled',
+        horizontalClass: 'rangeslider--horizontal',
+        verticalClass: 'rangeslider--vertical',
+        fillClass: 'rangeslider__fill',
+        handleClass: 'rangeslider__handle',
+
+        // Callback function
+        onInit: function() {},
+
+        // Callback function
+        onSlide: function(position, value) {},
+
+        // Callback function
+        onSlideEnd: function(position, value) {}
+    });
+    
     //----- OPEN
     $('[data-popup-open]').on('click', function(e) {
         var targeted_popup_class = jQuery(this).attr('data-popup-open');
